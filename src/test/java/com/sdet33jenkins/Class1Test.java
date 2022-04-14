98a9192bf80e959a35a41890d4ac8c465c36b0b0
Shivaay PC <Shivaay PC@DESKTOP-JFI584D>
2022-04-14 13:13:36 +0530

    practice first jenkins execution

---
+++ src/test/java/com/sdet33jenkins/Class1Test.java
@@ -0,0 +1,11 @@
+package com.sdet33jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	@Test
+	public void test1() {
+		System.out.println("step 1 from class1");
+	}
+
+}
---
+++ src/test/java/com/sdet33jenkins/Class3Test.java
@@ -0,0 +1,11 @@
+package com.sdet33jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	@Test
+	public void test1() {
+		System.out.println("step 1 from class3");
+	}
+
+}
