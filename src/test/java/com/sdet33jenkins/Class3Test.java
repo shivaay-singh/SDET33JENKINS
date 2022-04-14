204312629d5f1dc49afb21357ef717e3885d5fb6
Shivaay PC <Shivaay PC@DESKTOP-JFI584D>
2022-04-14 17:32:42 +0530

    jenkins commit

--- src/test/java/com/sdet33jenkins/Class3Test.java
+++ src/test/java/com/sdet33jenkins/Class3Test.java
@@ -5,7 +5,13 @@ import org.testng.annotations.Test;
 public class Class3Test {
 	@Test
 	public void test1() {
+		
+		
+		String browser = System.getProperty("BROWSER");
+		String url = System.getProperty("URL");
+		System.out.println(browser+"  "+url);
 		System.out.println("step 1 from class3");
+
 	}
 
 }
